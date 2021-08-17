f01fe5975f01cc1562c6aec20562f8ea5734a20d
edisson <osorioedisson08@gmail.com>
2021-08-16 20:44:55 -0500

    [ADD]fork.c

--- fork.c
+++ fork.c
@@ -4,28 +4,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int _strlen(char *str);
+/**
+ *_strlen - return the length of a string
+ *@s: character
+ *Return: length os string
+ */
+int _strlen(char *s)
+{
+    int i;
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        ;
+    }
+    return (i);
+}
+/**el tercer argumento es un doble puntero 
+ *osea un array de strings que contiene las variables de entorno
+ *en ellas el PATH donde budcaremos los ejecutables
+*/
 
 int main(void)
 {
     int i = 1;
-     char *prompt = "sh# ";
-      char *buffer = NULL;
-       size_t buffzise = 0;
-        int characters;
-         char* token[1024];
+    char *prompt = "$";
+    char *buffer = NULL;
+    size_t buffzise = 0;
+    int characters;
+    char *token[1024];
+    char *tmp;
 
-while(1)
-{
-    write(STDOUT_FILENO, prompt, _strlen(prompt));
-     characters = getline(&buffer, &buffzise, stdin);
-      token[0] = strtok(buffer, " ");
-       if (execve(token[0], token, NULL) == -1);
-       {
-           perror("Error");
-           i++;       
-       }
-       
-   }
-        free(buffer);  
+    while (1)
+    {
+        //the prompt function
+        write(STDOUT_FILENO, prompt, _strlen(prompt));
+        characters = getline(&buffer, &buffzise, stdin);
+        //tokenize the getline
+        token[0] = strtok(buffer, " \n"); 
+        tmp = buffer;
+        while (tmp)
+        {
+
+            tmp = strtok(NULL, " \n");
+            token[i] = tmp;
+           
+            i++;
+        }
+        //validate if is one built-in else built-in create fork and execute in child
+        if(execve(token[0], token, NULL) == -1)
+                perror("Error");
+    }
+    free(buffer);
+    return (0);
 }
